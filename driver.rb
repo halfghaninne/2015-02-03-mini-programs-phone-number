b06dff475f020b61615773411c031a967bfa0ed8
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-02-03 12:53:53 -0600

    Completed phone number formatter and driver, with documentation.

--- driver.rb
+++ driver.rb
@@ -1,12 +1,26 @@
 require 'pry'
 require_relative 'phone-number-formatter'
 
+# Class: Driver
+#
+# Tracks members of certain dinner clubs and their total payments.
+# Tracks their spending.
+# Tracks outings and attendance.
+# Allows individual members to "treat" the group and foot the bill.
+#
+# Attributes:
+# None.
+#
+# Public Methods:
+# + initialize
+
 class Driver
   
   def initialize
     puts "Please enter the number you would like to format:"
     number_to_format = gets.chomp.to_s
-    if number_to_format.length = 10
+    
+    if number_to_format.length == 10
       raw_answer = PhoneNumberFormatter.new(number_to_format)
       raw_answer.split_up_number
       formatted_answer = raw_answer.format
@@ -14,7 +28,10 @@ class Driver
     else
       "I'm sorry that's not a valid number. Try again."
     end
+    
     # come back to this loop and make it loop back through the program.
   end
   
 end
+
+binding.pry
--- phone-number-formatter.rb
+++ phone-number-formatter.rb
@@ -1,21 +1,57 @@
 require 'pry'
 
+# Class: PhoneNumberFormatter
+#
+# Transforms user-inputted number into a formatted string.
+#
+# Attributes:
+# + @number       - String: user-inputted number, returned as a string
+# + @area_code    - String: first three digits of user-inputted number
+# + @city_code    - String: middle three digits of user-inputted number
+# + @four_digits  - String: last four digits of user-inputted number
+#
+# Public Methods:
+# + split_up_number  
+# + format
+
 class PhoneNumberFormatter
   
   def initialize(number)
     number_as_string = number.to_s
     @number = number_as_string
+    #might not need multiple steps here, since Driver class converts user-input to String and then plugs in for parameter here.
+    #right now the program, when run through Driver, is doing the same step twice. 
   end
+
+  # Public: .split_up_number
+  # Splits @number string up into three separate parts.
+  # 
+  # Parameters: 
+  # None.
+  #
+  # Returns:
+  # Strings: @area_code, @city_code, @four_digits
+  #
+  # State Changes:
+  # None.
   
   def split_up_number
     @area_code = @number.byteslice(0, 3)
     @city_code = @number.byteslice(3, 3)
     @four_digits = @number.byteslice(6, 4)
-    # puts @area_code
-    # puts @city_code
-    # puts @four_digits
-    # puts @number
   end
+  
+  # Public: .format
+  # Puts a formatted number in a String, using instance variables created in .split_up_number.
+  # 
+  # Parameters: 
+  # None.
+  #
+  # Returns:
+  # String: String with desired formatting for the number.
+  #
+  # State Changes:
+  # None.
   
   def format
     puts "(#{@area_code}) #{@city_code}-#{@four_digits}"
